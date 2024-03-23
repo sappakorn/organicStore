519082ca5a373b3cd3bb033fc5cd8b292668ac04
Sappakorn26256 <sappakorn26256@gmail.com>
2024-03-24 00:38:27 +0700

    a
    a

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("hello")
+
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "node60666-vgtproject.th1.proen.cloud",
+  user: "root",
+  password: "OZIxac77163",
+  database: "myproject"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("You are connected!");
+});
+con.end();
